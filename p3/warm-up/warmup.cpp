2537bf5f103d0781f805226ffbdadb6d78d2a63b
William E. Skeith <wes@cs.ccny.cuny.edu>
2020-09-27 11:30:37 -0400

    Project 3 skeleton.

---
+++ p3/warm-up/warmup.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout;
+
+vector<vector<bool> > world = {
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
+int main()
+{
+	vector<vector<bool>> future(world); /* make future a copy of world. */
+	/* NOTE: the vector 'world' above corresponds to the contents
+	 * of ../res/tests/0.  TODO: apply the rules to the vector,
+	 * write the result to standard output, and compare with the
+	 * contents of ../tests/1. */
+	world = future;
+	return 0;
+}
